e0327fbf832f44bc72b163fa71a91bd62e357011
KevinRomeroBanegas <150660756+KevinRomeroBanegas@users.noreply.github.com>
2023-11-16 22:23:51 +0100

    Mensaje de campo vacio, modificar Paciente

--- DentiApps/interfaz/Interfaz_DentiLax/src/interfaz/Paciente.java
+++ DentiApps/interfaz/Interfaz_DentiLax/src/interfaz/Paciente.java
@@ -139,6 +139,22 @@ public class Paciente extends JDialog {
 		JButton btn_modificarCliente = new JButton("Modificar");
 		btn_modificarCliente.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
+			if(text_nombrePaciente.getText().isBlank()) {
+				JOptionPane.showMessageDialog(null, "Rellena el campo nombre");
+			}else if(text_dniPaciente.getText().isBlank()){
+				JOptionPane.showMessageDialog(null, "Rellena el campo dni");
+			}else if(text_direccionPaciente.getText().isBlank()) {
+				JOptionPane.showMessageDialog(null, "Rellena el campo direccion");
+			}else if(text_emailPaciente.getText().isBlank()) {
+				JOptionPane.showMessageDialog(null, "Rellena el campo email");
+			}else if(text_telefonoPaciente.getText().isBlank()) {
+				JOptionPane.showMessageDialog(null, "Rellena el campo telefono");
+			}else if(text_edadPaciente.getText().isBlank()){
+				JOptionPane.showMessageDialog(null, "Rellena el campo edad");
+			}else {
+				
+				JOptionPane.showMessageDialog(null, "Paciente Modificado");
+			}
 					
 			}
 		});
@@ -163,7 +179,7 @@ public class Paciente extends JDialog {
 				}else if(text_edadPaciente.getText().isBlank()){
 					JOptionPane.showMessageDialog(null, "Rellena el campo edad");
 				}else {
-					
+					JOptionPane.showMessageDialog(null, "Paciente Agregado");
 				}
 				
 			}
